0fdae1a91c228a879ac1febdb5706bb32f2aed6c
Mahesh <dananjayamahesh@gmail.com>
2023-05-29 19:00:17 +0100

    Update build.sh
    add another repo

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #install all packages
 # unabl to build https://github.com/tgockel/zookeeper-cpp.git
+#c++ wrapper for c api - https://github.com/yandaren/zk_cpp.git
+
 git clone https://github.com/Inphi/zookeeper-cpp.git
 cd zookeeper-cpp
 rm -rf CMakeLists.txt
